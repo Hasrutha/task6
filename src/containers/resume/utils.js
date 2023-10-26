e2199a5119ece3154d5536296162be5a6313c006
Hasrutha <144606134+Hasrutha@users.noreply.github.com>
2023-10-26 16:02:39 +0530

    Add files via upload

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,16 @@
+export const data = {
+    experience: [
+      {
+        title: "Consultant",
+        subTitle: "USA",
+        description:
+          " User Experience, Visual Design",
+      },
+    ],
+    education: [
+      {
+        title: "Bachelor of Technology",
+        subTitle: "VIT institute of technology, Amaravati.",
+      },
+    ],
+  };
